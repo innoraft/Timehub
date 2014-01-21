57a916650ef7c01c20f65197ec24f7dc8508f9f9
aman <aman.goel2452@gmail.com>
2014-01-21 17:18:12 +0530

    access push information by JSON file

--- webhook_data.php
+++ webhook_data.php
@@ -2,4 +2,4 @@
 
 $filename = "c:\webhook.txt";
 $payload = json_decode($_REQUEST['payload']);
-file_put_contents($filename, $e . ' ' . $payload, FILE_APPEND);
+file_put_contents($filename,$payload, FILE_APPEND);
