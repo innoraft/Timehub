71c3bede6da2e6c7100025fedaa28a9271e1e13d
aman <aman.goel2452@gmail.com>
2014-01-23 10:20:28 +0530

    access github information by JSON file

--- webhook_data.php
+++ webhook_data.php
@@ -1,6 +1,6 @@
 <?php
 
 $filename = "c:\webhook.txt";
-$fullPostData = file_get_contents('php://input');
-$payload = json_decode($fullPostData);
+//$fullPostData = file_get_contents('php://input');
+$payload = json_decode($_GET['payload']);
 file_put_contents($filename,$payload);
